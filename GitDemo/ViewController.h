c84dd77cbae9ace6e1058bdcd1d94b346ee4948d
fedor <apple@fedor.local>
2014-07-07 23:16:54 +0800

    second commit

---
+++ GitDemo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  GitDemo
+//
+//  Created by fedor on 14-7-7.
+//  Copyright (c) 2014年 vegegardon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ GitDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  GitDemo
+//
+//  Created by fedor on 14-7-7.
+//  Copyright (c) 2014年 vegegardon. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
